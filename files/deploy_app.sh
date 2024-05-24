2e2e2b8c73012fd655764df5bc231abbe581eb42
Hyunstone <hyun104467@naver.com>
2024-05-24 05:31:49 +0000

    Updated Image Size

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -6,13 +6,13 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>HYUN!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
-  <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
+  <center><img style="width:100%; margin: 0 auto" src="https://github.com/Team-MOGAK/MOGAK_Spring/assets/110045522/751e1178-a55f-42f9-b12b-4cceca0b7b3f"></img></center>
+  <center><h2>Hyun's World!</h2></center>
   Welcome to ${PREFIX}'s app. Replace this text with your own.
   <!-- END -->
 
